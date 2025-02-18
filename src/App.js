2d63240530902d7fc8add4c18e81ff41a5e73a9c
Cameron <cameron.cw@outlook.com>
2025-02-18 13:16:57 +0000

    Added Home & Posts Pages

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from './pages/Home';
+import Posts from './pages/Posts';
 
 function App() {
   return (
     <Router>
     <div className="App">
      <Routes>
-      <Route></Route>
-      <Route></Route>
+      <Route path="/" element={<Home />}></Route>
+      <Route path=":id" element={<Posts />}></Route>
      </Routes>
     </div>
     </Router>
