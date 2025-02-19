7eee2decc344bdc0d438a2eedd378f78ec841c80
Cameron <cameron.cw@outlook.com>
2025-02-19 12:45:09 +0000

    Add useParams to Posts page

--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import { useParams } from 'react-router-dom';
 
 export default function Posts() {
+  const { id } = useParams();
+  console.log(id);
   return (
     <div>
-        Posts
+        {id}
     </div>
   )
 }
-
