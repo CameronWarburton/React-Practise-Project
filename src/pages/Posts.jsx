4b2e7c61e1562f38784c4746ae6d9efdd2d27be7
Cameron <cameron.cw@outlook.com>
2025-02-23 14:44:33 +0000

    Fetching Posts from API and making the userId dynamic

--- src/pages/Posts.jsx
+++ src/pages/Posts.jsx
@@ -1,13 +1,21 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom';
+import axios from 'axios';
 
 const Posts = () => {
   const { id } = useParams();
+  const [posts, setPosts] = useState([]);
   
-  useEffect(() => {})
+  useEffect(() => {
+    async function fetchPosts() {
+      const { data } = await axios.get(`https://jsonplaceholder.typicode.com/posts?userId=${id}`);
+      setPosts(data);
+    }
+    fetchPosts();
+  }, [])
   return (
     <div>
-        {id}
+        {posts.map(post => <div>{post.title}</div>)}
     </div>
   )
 }
