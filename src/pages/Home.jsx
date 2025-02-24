a9ec970d38ecca4cad28a81e57501e3d58591ac7
Cameron <cameron.cw@outlook.com>
2025-02-24 12:41:15 +0000

    Made a start on home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from "react";
 
 export default function Home() {
   return (
-    <div>
-        Home
+    <div className="container">
+      <div className="row">
+        <div className="user-list">
+          <div className="user">
+            <div className="user-card">
+              <div className="user-card__container">
+                <h3>user.name</h3>
+                <p>
+                  <b>Email:</b> user.email
+                </p>
+                <p>
+                  <b>Phone:</b> user.phone
+                </p>
+                <p>
+                  <b>Website:</b>
+                  user.website
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
